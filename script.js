930c385fbac6b9158911ca604204d2566b962faa
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-15 23:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const makeChange = (c) => {
-  // your name here
+  let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  quarters = Math.floor(c / 25);
+  c %= 25;
+
+  dimes = Math.floor(c / 10);
+  c %= 10;
+
+  nickels = Math.floor(c / 5);
+  c %= 5;
+
+  pennies = c;
+
+  return {
+    q: quarters,
+    d: dimes,
+    n: nickels,
+    p: pennies,
+  };
 };
 
-// Do not the change the code below
+// Example usage
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(parseInt(c))));
